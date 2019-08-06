ca9d3173ad88aa5516dfd2c1d61333a5c053de48
Ken Mueller <kenmueller0@gmail.com>
2019-08-05 22:14:19 -0700

    Adding new constants

---
+++ constants.py
@@ -0,0 +1,21 @@
+# Network structure
+
+# Average number of computers in a local network
+AVERAGE_LOCAL_NETWORK_SIZE = 14
+
+# Average number of computers a single usb drive will be plugged in to
+AVERAGE_USB_DRIVE_SHARING_NETWORK_SIZE = 5
+
+# Average number of local networks contained in a single "big" network
+AVERAGE_NUMBER_OF_LOCAL_NETWORKS_IN_BIG_NETWORK = 3
+
+# Total number of big networks
+NUMBER_OF_BIG_NETWORKS = 25
+
+# Infection rate
+
+# The probability, for each wave, that if you're a neighbor of an infected computer, you will be infected as well
+USB_TRANSMISSION_PROBABILITY = 0.1
+
+# The probability, for each wave, that if you're on the same local network as an infected computer, you will be infected as well
+LOCAL_NETWORK_TRANSMISSION_PROBABILITY = 0.2
