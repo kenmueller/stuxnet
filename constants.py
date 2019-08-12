715d919d1b0d57017103a075302d1a311027e17c
Ken Mueller <kenmueller0@gmail.com>
2019-08-12 01:39:32 -0700

    added constants

--- constants.py
+++ constants.py
@@ -1,10 +1,13 @@
 # Network structure
 
-# Average number of computers in a local network
-AVERAGE_LOCAL_NETWORK_SIZE = 14
-
 # Average number of computers a single usb drive will be plugged in to
 AVERAGE_USB_DRIVE_SHARING_NETWORK_SIZE = 5
+
+# Average number of computers in a single wired network (Ethernet)
+AVERAGE_LOCAL_WIRED_NETWORK_SIZE = 10
+
+# Average number of computers in a single wireless network
+AVERAGE_LOCAL_WIRELESS_NETWORK_SIZE = 30
 
 # Average number of local networks contained in a single "big" network
 AVERAGE_NUMBER_OF_LOCAL_NETWORKS_IN_BIG_NETWORK = 3
@@ -12,16 +15,39 @@ AVERAGE_NUMBER_OF_LOCAL_NETWORKS_IN_BIG_NETWORK = 3
 # Total number of big networks
 NUMBER_OF_BIG_NETWORKS = 25
 
-# Infection rate
+# Transmission probabilities
 
-# The probability, for each wave, that if you're a neighbor of an infected computer, you will be infected as well
+# The probability, for each wave, that an infected computer will infect its neighbor via USB
 USB_TRANSMISSION_PROBABILITY = 0.1
 
-# The probability, for each wave, that if you're on the same local network as an infected computer, you will be infected as well
-LOCAL_NETWORK_TRANSMISSION_PROBABILITY = 0.2
+# The probability, for each wave, that an infected computer will infect its neighbor via LNK vulnerability
+LNK_TRANSMISSION_PROBABILITY = 0.5
 
-# The probability, for each wave, that you will get infected with Stuxnet's Windows autoupdate hack
-WINDOWS_AUTO_UPDATE_TRANSMISSION_PROBABILITY = 0.1
+# The probability, for each wave, that an infected computer will infect its neighbor via autorun vulnerability
+AUTORUN_TRANSMISSION_PROBABILITY = 0.5
+
+# The probability, for each wave, that an infected computer will infect its neighbor via print spooler vulnerability
+PRINT_SPOOLER_TRANSMISSION_PROBABILITY = 0.5
+
+# The probability, for each wave, that an infected computer will infect its neighbor via WinCC
+WINCC_TRANSMISSION_PROBABILITY = 0.5
+
+# The probability, for each wave, that an infected computer will infect its neighbor via a network share
+NETWORK_SHARES_TRANSMISSION_PROBABILITY = 0.5
+
+# The probability, for each wave, that an infected computer will infect its neighbor via SMB
+SMB_TRANSMISSION_PROBABILITY = 0.5
+
+# The probability, for each wave, that a given computer will get infected from a windows auto update
+WINDOWS_AUTO_UPDATE_TRANSMISSION_PROBABILITY = 0.5
+
+# Limits
+
+# The number of milliseconds a wave corresponds to
+WAVE_LENGTH = 86400000
 
 # The number of computers stuxnet can infect before it turns itself off
 INFECTION_LIMIT = 3
+
+# The number of milliseconds until stuxnet self destructs
+INFECTION_DURATION = 94608000000
