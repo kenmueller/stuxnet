f8f3f480a6ea806b0a7242bd5dcbfd10f345bfcd
Ken Mueller <kenmueller0@gmail.com>
2019-08-13 14:32:06 -0700

    removed unused import

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from create_graph import create_graph
-from edge_type import EdgeType
 
 if __name__ == '__main__':
 	nx.draw(create_graph(), with_labels=True)
