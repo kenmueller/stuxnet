1f6352738c07f7d552ce21a131e50a8eb71f4a34
Ken Mueller <kenmueller0@gmail.com>
2019-08-13 14:21:51 -0700

    removed +1

--- create_graph.py
+++ create_graph.py
@@ -3,6 +3,8 @@ from numpy.random import normal
 from constants import *
 from EdgeType import EdgeType
 
+NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS = NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS
+
 def network_size_for_edge_type(edge_type: int):
 	return max(0, round(normal(NETWORK_SIZES[edge_type])))
 
@@ -13,8 +15,8 @@ def create_graph():
 	graph = nx.Graph()
 	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS):
 		add_network(graph, EdgeType.LOCAL_WIRED, i)
-	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS, NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS):
+	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS, NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS):
 		add_network(graph, EdgeType.LOCAL_WIRELESS, i)
-	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS):
-		graph.add_edge(i, NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS + 1)
+	for i in range(NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS):
+		graph.add_edge(i, NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS)
 	return graph
