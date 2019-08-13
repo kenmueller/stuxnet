516d4a7b37ed8fe23ec5645f158c6a913001d480
Ken Mueller <kenmueller0@gmail.com>
2019-08-13 16:14:28 -0700

    return type signatures

--- create_graph.py
+++ create_graph.py
@@ -5,13 +5,13 @@ from edge_type import EdgeType
 
 NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS = NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS
 
-def network_size_for_edge_type(edge_type: EdgeType):
+def network_size_for_edge_type(edge_type: EdgeType) -> int:
 	return max(0, round(normal(NETWORK_SIZES[edge_type])))
 
 def add_network(graph: nx.Graph, edge_type: EdgeType, num: int):
 	graph.add_edges_from([(num, (num, i)) for i in range(network_size_for_edge_type(edge_type))], edge_type=edge_type)
 
-def create_graph():
+def create_graph() -> nx.Graph:
 	graph = nx.Graph()
 	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS):
 		add_network(graph, EdgeType.LOCAL_WIRED, i)
