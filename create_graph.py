315184c1ebc3af1cf5f73f9b0214c53564f8cda0
Ken Mueller <kenmueller0@gmail.com>
2019-08-13 14:36:33 -0700

    fixed types

--- create_graph.py
+++ create_graph.py
@@ -5,10 +5,10 @@ from edge_type import EdgeType
 
 NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS = NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS
 
-def network_size_for_edge_type(edge_type: int):
+def network_size_for_edge_type(edge_type: EdgeType):
 	return max(0, round(normal(NETWORK_SIZES[edge_type])))
 
-def add_network(graph: nx.Graph, edge_type: int, num: int):
+def add_network(graph: nx.Graph, edge_type: EdgeType, num: int):
 	graph.add_edges_from([(num, (num, i)) for i in range(network_size_for_edge_type(edge_type))], edge_type=edge_type)
 
 def create_graph():
