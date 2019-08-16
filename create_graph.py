1566ec10c161532a1de327f617c84a3bfebd19b3
Ken Mueller <kenmueller0@gmail.com>
2019-08-16 02:28:42 -0700

    shows graph with usb sharing

--- create_graph.py
+++ create_graph.py
@@ -1,9 +1,10 @@
 import networkx as nx
 from numpy.random import normal
+from random import sample
 from constants import *
 from edge_type import EdgeType
 
-NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS = NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS
+NUMBER_OF_LOCAL_NETWORKS = NUMBER_OF_LOCAL_WIRED_NETWORKS + NUMBER_OF_LOCAL_WIRELESS_NETWORKS
 
 def network_size_for_edge_type(edge_type: EdgeType) -> int:
 	return max(0, round(normal(NETWORK_SIZES[edge_type])))
@@ -15,8 +16,16 @@ def create_graph() -> nx.Graph:
 	graph = nx.Graph()
 	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS):
 		add_network(graph, EdgeType.LOCAL_WIRED, i)
-	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS, NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS):
+	for i in range(NUMBER_OF_LOCAL_WIRED_NETWORKS, NUMBER_OF_LOCAL_NETWORKS):
 		add_network(graph, EdgeType.LOCAL_WIRELESS, i)
-	for i in range(NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS):
-		graph.add_edge(i, NUMBER_OF_LOCAL_WIRED_AND_WIRELESS_NETWORKS)
+	for i in range(NUMBER_OF_USB_SHARING_NETWORKS):
+		for router_node in sample(range(NUMBER_OF_LOCAL_NETWORKS), max(0, round(normal(NETWORK_SIZES[EdgeType.USB_SHARED]['NETWORKS'])))):
+			child_nodes = len(list(graph.neighbors(router_node)))
+			nodes = sample(range(child_nodes), max(0, min(round(normal(NETWORK_SIZES[EdgeType.USB_SHARED]['NODES_IN_NETWORK'])), child_nodes)))
+			for node in nodes:
+				graph.add_edge(f'person_{i}', (router_node, node), edge_type=EdgeType.USB_SHARED)
+		for disconnected_computer in range(max(0, round(normal(NETWORK_SIZES[EdgeType.USB_SHARED]['DISCONNECTED'])))):
+			graph.add_edge(f'person_{i}', f'disconnected_{disconnected_computer}', edge_type=EdgeType.USB_SHARED)
+	for i in range(NUMBER_OF_LOCAL_NETWORKS):
+		graph.add_edge(i, NUMBER_OF_LOCAL_NETWORKS)
 	return graph
