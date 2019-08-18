2aec97c9ec75c0574bae61e734defdcc2d40e415
Ken Mueller <kenmueller0@gmail.com>
2019-08-17 20:06:01 -0700

    wave.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from create_graph import create_graph
+from wave import new_wave
+
+def draw_graph(graph: nx.Graph, **kwargs):
+	nx.draw(graph, with_labels=True)
+	plt.show(**kwargs)
 
 if __name__ == '__main__':
-	nx.draw(create_graph(), with_labels=True)
+	graph = create_graph()
+	draw_graph(graph, block=False)
+	# TODO: Create waves and display data
 	plt.show()
---
+++ wave.py
@@ -0,0 +1,4 @@
+import networkx as nx
+
+def new_wave(graph: nx.Graph):
+	pass
