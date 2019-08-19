b53382b63178b8cac963bbc06a4cc9c5f7a67bbc
Ken Mueller <kenmueller0@gmail.com>
2019-08-19 02:55:15 -0700

    nodes -> node

--- src/node.py
+++ src/node.py
@@ -2,7 +2,7 @@ import networkx as nx
 from node_type import NodeType
 
 def is_node_infected(graph: nx.Graph, node) -> bool:
-	return graph.nodes[node].get('infected', False)
+	return graph.node[node].get('infected', False)
 
 def set_node_infected(graph: nx.Graph, node, infected: bool):
 	graph.node[node]['infected'] = infected
