afe42208ce7e0e58f422716f32cdd81e013be9c2
Ken Mueller <kenmueller0@gmail.com>
2019-08-19 08:38:29 -0700

    passes through router and main, but doesn't infect them

--- src/node.py
+++ src/node.py
@@ -14,10 +14,7 @@ def set_node_type(graph: nx.Graph, node, node_type: NodeType):
 	graph.node[node]['node_type'] = node_type
 
 def local_network_neighbors(graph: nx.Graph, node):
-	return filter(
-		lambda neighbor_node: get_node_type(neighbor_node) == NodeType.COMPUTER,
-		graph.neighbors(filter(
-			lambda neighbor_node: get_node_type(neighbor_node) == NodeType.ROUTER,
-			graph.neighbors(node)
-		)[0])
-	)
+	return list(filter(
+		lambda adjacency: get_node_type(graph, adjacency[0]) == NodeType.COMPUTER,
+		graph[list(filter(lambda neighbor_node: get_node_type(graph, neighbor_node) == NodeType.ROUTER, graph.neighbors(node)))[0]].items()
+	))
--- src/wave.py
+++ src/wave.py
@@ -1,16 +1,20 @@
 import networkx as nx
 from random import random
 from constants import *
-from node import is_node_infected, set_node_infected
+from node import is_node_infected, set_node_infected, get_node_type, local_network_neighbors
 from edge_type import EdgeType
+from node_type import NodeType
 
 def should_infect_node_with_probability(probability: float) -> bool:
 	return random() < probability
 
+def node_type_is_not_router_or_main(graph: nx.Graph, node):
+	node_type = get_node_type(graph, node)
+	return not (node_type == NodeType.ROUTER or node_type == NodeType.MAIN)
+
 def new_wave(graph: nx.Graph):
-	edge_types = nx.get_edge_attributes(graph, 'edge_type')
 	for node in filter(lambda node: is_node_infected(graph, node), graph.nodes):
-		for neighbor_node, edge_data in graph.adj[node].items():
+		for neighbor_node, edge_data in list(filter(lambda adjacency: node_type_is_not_router_or_main(graph, adjacency[0]), graph.adj[node].items())) + (local_network_neighbors(graph, node) if get_node_type(graph, node) == NodeType.COMPUTER else []):
 			edge_type = edge_data['edge_type']
 			if edge_type == EdgeType.LOCAL_WIRED and (
 				should_infect_node_with_probability(PRINT_SPOOLER_TRANSMISSION_PROBABILITY) or \
